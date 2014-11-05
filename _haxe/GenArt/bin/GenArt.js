407226e220bd44c2b70fc3e2c13075b18fdaa952
Matthijs Kamstra aka [mck] <matthijs@fonkmobile.nl>
2014-11-05 23:43:14 +0100

    added new icons, javascript, more buttons, social metadata

---
+++ _haxe/GenArt/bin/GenArt.js
@@ -0,0 +1,136 @@
+(function () { "use strict";
+var Main = function() {
+	Main._window = window;
+	Main._document = window.document;
+	new $(function() {
+		if(new $("body").hasClass("post")) {
+			haxe.Log.trace("post",{ fileName : "Main.hx", lineNumber : 24, className : "Main", methodName : "new"});
+			return;
+		}
+		var count = new $("section");
+		var array = [];
+		var _g1 = 0;
+		var _g = count.length;
+		while(_g1 < _g) {
+			var i = _g1++;
+			var htmlEl = count[i];
+			array.push(htmlEl);
+		}
+		new $("section img").wrap("<div class='preview'></div>");
+		new $(".preview").prepend("<span class='cutline'/>");
+		var w = new $("section img").width();
+		new $(".preview").height(w * .66);
+		haxe.Log.trace("document ready: section: " + Std.string(count),{ fileName : "Main.hx", lineNumber : 44, className : "Main", methodName : "new"});
+		haxe.Log.trace(array.length,{ fileName : "Main.hx", lineNumber : 45, className : "Main", methodName : "new", customParams : [array[3]]});
+	});
+};
+Main.__name__ = true;
+Main.main = function() {
+	var main = new Main();
+};
+var Std = function() { };
+Std.__name__ = true;
+Std.string = function(s) {
+	return js.Boot.__string_rec(s,"");
+};
+var haxe = {};
+haxe.Log = function() { };
+haxe.Log.__name__ = true;
+haxe.Log.trace = function(v,infos) {
+	js.Boot.__trace(v,infos);
+};
+var js = {};
+js.Boot = function() { };
+js.Boot.__name__ = true;
+js.Boot.__unhtml = function(s) {
+	return s.split("&").join("&amp;").split("<").join("&lt;").split(">").join("&gt;");
+};
+js.Boot.__trace = function(v,i) {
+	var msg;
+	if(i != null) msg = i.fileName + ":" + i.lineNumber + ": "; else msg = "";
+	msg += js.Boot.__string_rec(v,"");
+	if(i != null && i.customParams != null) {
+		var _g = 0;
+		var _g1 = i.customParams;
+		while(_g < _g1.length) {
+			var v1 = _g1[_g];
+			++_g;
+			msg += "," + js.Boot.__string_rec(v1,"");
+		}
+	}
+	var d;
+	if(typeof(document) != "undefined" && (d = document.getElementById("haxe:trace")) != null) d.innerHTML += js.Boot.__unhtml(msg) + "<br/>"; else if(typeof console != "undefined" && console.log != null) console.log(msg);
+};
+js.Boot.__string_rec = function(o,s) {
+	if(o == null) return "null";
+	if(s.length >= 5) return "<...>";
+	var t = typeof(o);
+	if(t == "function" && (o.__name__ || o.__ename__)) t = "object";
+	switch(t) {
+	case "object":
+		if(o instanceof Array) {
+			if(o.__enum__) {
+				if(o.length == 2) return o[0];
+				var str = o[0] + "(";
+				s += "\t";
+				var _g1 = 2;
+				var _g = o.length;
+				while(_g1 < _g) {
+					var i = _g1++;
+					if(i != 2) str += "," + js.Boot.__string_rec(o[i],s); else str += js.Boot.__string_rec(o[i],s);
+				}
+				return str + ")";
+			}
+			var l = o.length;
+			var i1;
+			var str1 = "[";
+			s += "\t";
+			var _g2 = 0;
+			while(_g2 < l) {
+				var i2 = _g2++;
+				str1 += (i2 > 0?",":"") + js.Boot.__string_rec(o[i2],s);
+			}
+			str1 += "]";
+			return str1;
+		}
+		var tostr;
+		try {
+			tostr = o.toString;
+		} catch( e ) {
+			return "???";
+		}
+		if(tostr != null && tostr != Object.toString) {
+			var s2 = o.toString();
+			if(s2 != "[object Object]") return s2;
+		}
+		var k = null;
+		var str2 = "{\n";
+		s += "\t";
+		var hasp = o.hasOwnProperty != null;
+		for( var k in o ) {
+		if(hasp && !o.hasOwnProperty(k)) {
+			continue;
+		}
+		if(k == "prototype" || k == "__class__" || k == "__super__" || k == "__interfaces__" || k == "__properties__") {
+			continue;
+		}
+		if(str2.length != 2) str2 += ", \n";
+		str2 += s + k + " : " + js.Boot.__string_rec(o[k],s);
+		}
+		s = s.substring(1);
+		str2 += "\n" + s + "}";
+		return str2;
+	case "function":
+		return "<function>";
+	case "string":
+		return o;
+	default:
+		return String(o);
+	}
+};
+String.__name__ = true;
+Array.__name__ = true;
+Main.main();
+})();
+
+//# sourceMappingURL=GenArt.js.map
---
+++ _haxe/GenArt/src/Main.hx
@@ -0,0 +1,55 @@
+package;
+
+import js.html.HtmlElement;
+import js.html.DOMWindow;
+import js.html.*;
+// import js.JQuery;
+import jQuery.*;
+import mconsole.*;
+
+class Main
+{
+	
+	static public var _window:DOMWindow;
+	static public var _document:Dynamic;
+
+	function new()
+	{
+		_window   = js.Browser.window;
+		_document = js.Browser.document;
+		
+		new JQuery( function():Void 
+		{
+			if(new JQuery('body').hasClass('post')) 
+			{
+				trace ('post');
+				return;
+			}
+
+			// when document is ready
+			var count = new JQuery('section');
+
+			var array:Array<HtmlElement> = [];
+			for (i in 0...count.length)
+			{
+				var htmlEl:HtmlElement = cast (count[i]);
+				array.push (htmlEl);
+				// new js.JQuery(_document.body).prepend ('<a href="#'+htmlEl.id+'">'+htmlEl.id+'</a>');
+			}
+			new JQuery("section img" ).wrap( "<div class='preview'></div>" );
+			new JQuery(".preview" ).prepend( "<span class='cutline'/>" );
+
+			var w = new JQuery('section img').width();
+			new JQuery('.preview').height(w*.66);
+
+			trace('document ready: section: ' + count);
+			trace (array.length , array[3]);
+		
+		});
+	}
+
+    static public function main()
+    {
+       var main = new Main();
+	}
+}
